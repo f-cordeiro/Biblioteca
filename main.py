9ec36097c5d87e2c4a004a68e857697756bb8007
Felipe Cordeiro <olifelipecordeiro@gmail.com>
2025-10-03 10:10:36 -0300

    ETAPA 1 - Criação da tabela

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ import sqlite3
 conexao = sqlite3.connect("biblioteca.db")
 
 cursor = conexao.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS livros (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo TEXT NOT NULL,
+    autor TEXT NOT NULL,
+    ano INTEGER,
+    disponibilidade CHAR(1) CHECK(disponibilidade IN('S','N'))
+    )              
+""")
